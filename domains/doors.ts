18f6c7670573a1d2fe1f4df2b207fcf8dbcbc9bf
Gabriel D. Padua <gabriel.d.padua21@gmail.com>
2022-08-25 21:21:04 -0300

    ♻️refactor: add cursor pointer and param with present

--- domains/doors.ts
+++ domains/doors.ts
@@ -1,9 +1,9 @@
 import DoorModel from "../model/Door";
 
-export function createDoors(quantity: number, selected: number): DoorModel[] {
+export function createDoors(quantity: number, withPresent: number): DoorModel[] {
     const doors = Array.from({ length: quantity }, (_, i) => {
       const number = i + 1;
-      const havePresent = number === selected;
+      const havePresent = number === withPresent;
       return new DoorModel(number, havePresent);
     });
     return doors;
